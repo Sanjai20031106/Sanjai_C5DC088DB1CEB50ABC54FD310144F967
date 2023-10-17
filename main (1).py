67e0c47610a98e4e0cacb87058b769989d8e6e6e
Sanjai20031106 <148200758+Sanjai20031106@users.noreply.github.com>
2023-10-17 14:09:00 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,10 @@
+def ISLP(y):
+    if(y % 4 == 0):
+      print("true");
+    else:
+      print("false");
+year=int(input("Enter the year:"));
+print(ISLP(year));
+
+
+
---
+++ main (3).py
@@ -0,0 +1,8 @@
+
+def ISLP(y):
+    if(y % 4 == 0):
+      print("true");
+    else:
+      print("false");
+year=int(input("Enter the year:"));
+print(ISLP(year));
